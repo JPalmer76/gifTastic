b7d2224a4f8242098c4a87b6b4489b1c9cafb4f4
Jonny <jpdadof3@gmail.com>
2019-06-05 18:20:04 -0500

    working functionality, but needs styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,74 +1,94 @@
 $(document).ready(function() {
   // Initial array of Gifs
-  var topics = ["honey Boo Boo", "grumpy cat", "puking", "dogs", "cats"];
+  var topics = ["honey boo boo", "grumpy cat", "dumb and dumber"];
+  renderButtons();
 
-  // GLobal variables
-
-  var input = $("#gif-input");
-  // var submit = $("#submit");
-  // var apiKey = "SCTZFGIpnkbisLwvI5TonrVEsJTSuiwG";
-
-  // render buttons
+  // render buttons function
   function renderButtons() {
     $("#buttons-view").empty();
     for (var i = 0; i < topics.length; i++) {
       var a = $("<button>");
-      a.addClass("Gifs");
-      a.attr("data-name", topics[i]);
-      a.text(topics[i]);
-      $("#buttons-view").append(a);
+    a.addClass("gifs btn btn-success");
+    a.attr("data-name", topics[i]);
+    a.text(topics[i]);
+    $("#buttons-view").append(a);
+    
     }
   }
 
-  // Get inout value from search on "submit"
-   
 
-  $("#add-gif").on("click", function(event) {
+// add gif (input search) on click function
+  $("#add-gif").on("click", function(event){
     event.preventDefault();
-    var gifPicture = $(this).attr("data-name");
+    var input = $("#gif-input").val().trim();
+    console.log(input);
+    var a = $("<button>");
+    a.addClass("gifs btn btn-success");
+    a.attr("data-name", input);
+    a.text(input);
+    $("#buttons-view").append(a);
+    $("#gif-input").val("");
+    
+    
+
+  })
+  // Get input value from search on "submit"
+  // make api call with ajax and adding the value
+   $("#buttons-view").on("click", ".gifs", function(event) {
+    event.preventDefault();
+    var gifPicture = $(this).attr("data-name")
     var queryURL =
-      "https://api.giphy.com/v1/gifs/search?q=" +
-      gifPicture +
-      "&api_key=OcU56NBTWY5dF3inq3DPRQNfBYZbrgkN&limit=10";
+      "https://api.giphy.com/v1/gifs/search?api_key=SCTZFGIpnkbisLwvI5TonrVEsJTSuiwG&q=" + gifPicture + "&limit=10&offset=0&rating=G&lang=en";
       console.log(queryURL)
    
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      console.log(response)
       var results = response.data;
-      var gif = $(input).val().trim();
+      console.log(results)
+      var gif = $("#gif-input").val().trim();
+     
       topics.push(gif);
-      renderButtons();
-      
-      for (var i = 0; i < results.length; i++) {
+     
+      // loop throught the response(oject)
+      for(let i = 0; i < response.data.length; i++) {
+       console.log(i);
         var gifDiv = $("<div>");
         var p = $("<p>").text("Rating: " + results[i].rating);
         var gifImage = $("<img>");
-        gifImage.attr("src", results[i].images.fixed_height.url);
+        gifImage.attr("src", results[i].images.fixed_height_small_still.url);
+        gifImage.attr("data-still", results[i].images.fixed_height_small_still.url);
+        gifImage.attr("data-animate", results[i].images.fixed_height_small.url);
+        gifImage.attr("state", "still");
+        gifImage.addClass("gify");
         gifDiv.append(p);
         gifDiv.append(gifImage);
-
+        
         $("#gifs-appear-here").prepend(gifDiv);
       }
-    });
+      }) 
+      
+    
   });
-  renderButtons();
+
+  $(document).on("click", ".gify", function() {
+
+   
+    var state = $(this).attr("state");
+    console.log(state)
+    
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("state", "still");
+    }
+  });
+  
+  
 });
 
 
 
-// $.ajax({
-//   url: giphyUrl,
-//   method: "GET"
-// }).then(function (r) {
-//   for (var i = 0; i < r.data.length; i++) {
-//     var url = r.data[i].images.fixed_width.url;
-//     console.log(url);
-//     var giphyImg = $("<img>");
-//     giphyImg.attr("src", url);
-//     $("#imgId").append(giphyImg);
-//   }
-
-// })
